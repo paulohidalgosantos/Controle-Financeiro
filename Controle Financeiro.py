5140bdf17c415d52040bafd98d6ba5c2c449d6d7
Paulo <paulo_hidalgo_santos@live.com>
2025-09-04 09:05:37 -0300

    att

--- Controle Financeiro.py
+++ Controle Financeiro.py
@@ -19,7 +19,7 @@ import webbrowser
 import urllib.request
 import locale
 
-VERSAO_ATUAL = "1.1.2"
+VERSAO_ATUAL = "1.1.3"
 def buscar_atualizacao():
     """Verifica se existe uma nova versão e chama o updater se necessário."""
     try:
